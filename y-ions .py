2c87d6ae7a360f7f4266f7ddf03af9cf0d2abd50
dinamohamed45 <94751963+dinamohamed45@users.noreply.github.com>
2021-12-12 19:49:47 +0200

    Add files via upload

---
+++ y-ions .py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+# Y-ion spectrum on yeast
+dig = ProteaseDigestion()
+dig.getEnzymeName()
+bsa = "".join([l.strip() for l in open("yeast.fasta").readlines()[1:]])
+bsa = AASequence.fromString(bsa)
+result = []
+dig.digest(bsa, result)
+peptides = [AASequence.fromString(s.toString()) for s in result]
+
+for peptide in peptides:
+    tsg = TheoreticalSpectrumGenerator()
+    spec1 = MSSpectrum()  
+    p = Param()
+    p.setValue("add_b_ions", "false")
+    p.setValue("add_metainfo", "true")
+    tsg.setParameters(p)
+    tsg.getSpectrum(spec1, peptide, 1, 1) # charge range 1:1
+    print("Spectrum 1 of", peptide, "has", spec1.size(), "peaks.")
+    for ion, peak in zip(spec1.getStringDataArrays()[0], spec1):
+        print(ion.decode(), "is generated at m/z", peak.getMZ())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
